c97434248b8597674f8788b285e5b8e8912e7465
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-10-24 12:24:27 +1100

    Video Game Controller implemented

--- obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cfe1e2afa2d9f96e94e8ebe457162b7325a04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
